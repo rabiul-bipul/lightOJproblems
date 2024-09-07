3d1b06a99f02503df722086fabfe82cd6ded64e6
Rabiul Islam Bipul <132159770+rabiul-bipul@users.noreply.github.com>
2024-09-07 20:00:15 +0600

    Create Opposite Task.cpp

---
+++ Opposite Task.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  int t;
+  cin >> t;
+
+  while(t--){
+    int n;
+    cin >> n;
+
+    if(n > 10){
+        cout << 10 <<" " << n - 10 << endl;
+    }
+    else{
+        cout << 0 <<" " << n << endl;
+    }
+  }   
+    return 0;
+}
