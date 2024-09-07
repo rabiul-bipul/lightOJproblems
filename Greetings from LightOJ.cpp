3bbbdff53e4a63226fc71f1871c87ffe847cc67e
Rabiul Islam Bipul <132159770+rabiul-bipul@users.noreply.github.com>
2024-09-07 19:41:00 +0600

    Create Greetings from LightOJ.cpp

---
+++ Greetings from LightOJ.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Returns the total number of problems.
+ *
+ * @param a denotes the number of problems in the first computer
+ * @param b denotes the number of problems in the second computer
+ */
+int sum(int a, int b) {
+    // Implement this method
+    int sum = (a+b);
+    return sum;
+}
+
+/**
+ * Takes care of the problem input and output.
+ */
+int main() {
+    int cases;
+    scanf("%d", &cases);
+    for (int caseno = 1; caseno <= cases; ++caseno) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("Case %d: %d\n", caseno, sum(a, b));
+    }
+    return 0;
+}
